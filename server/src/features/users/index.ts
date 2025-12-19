8e11921a8d7cd23115b215916c6b080df6c5738c
brtprivate <dev2.brtmultisoftware@gmail.com>
2025-12-20 03:10:59 +0530

    Delete server directory

--- server/src/features/users/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all user-related modules
-export { User } from './models/user.model';
-export { userService } from './services/user.service';
-export { userController } from './controllers/user.controller';
-export { default as userRoutes } from './routes/user.routes';
-export * from './types/user.types';
--- server/src/features/withdrawals/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './models/withdrawal.model';
-export * from './services/withdrawal.service';
-export * from './controllers/withdrawal.controller';
-export * from './types/withdrawal.types';
-
-
-
